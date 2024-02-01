2a81c5bf70c60df5b52d95e1450bd657e3a78c2b
German <grehko-german@mail.ru>
2024-02-01 16:18:07 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 14700; // стоимость билета
+        int bonus = ticket / 20; // бонусные мили
+        System.out.println("Количество миль" + bonus);
+
+    }
+}
